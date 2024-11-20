788cafa1afd33658f15e7b6ab22adeab27fa782e
Adarsh-xar <adarsh.kumar@xarterian.com>
2024-11-20 11:28:35 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    sonarqube = {
+      source = "jdamata/sonarqube"
+    }
+  }
+}
+
+provider "sonarqube" {
+    token  = "sqa_e5254aa3c164ce7c0f721302926d8599d9594db7"
+    host   = "http://127.0.0.1:9000"
+}
+
+
+# provider "sonarqube" {
+#   user   = var.sonarqube_user
+#   pass   = var.sonarqube_password
+#   host   = var.sonarqube_host
+#   token  = var.sonarqube_token  
+# }
+
+resource "sonarqube_project" "my_project" {
+  name       = var.project_name
+  project    = var.project_key
+  visibility = "public"
+}
+
+resource "sonarqube_qualitygate" "main" {
+  name = var.qualitygate_name
+
+  condition {
+    metric    = "new_coverage"
+    op        = "LT"
+    threshold = var.coverage_threshold
+  }
+}
+
+resource "sonarqube_qualitygate_project_association" "main" {
+  gatename   = sonarqube_qualitygate.main.id
+  projectkey = sonarqube_project.my_project.project
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "sonarqube_user" {
+  description = "The username to authenticate to SonarQube"
+  type        = string
+  default     = "admin"  
+}
+
+variable "sonarqube_password" {
+  description = "The password to authenticate to SonarQube"
+  type        = string
+  sensitive   = true
+}
+
+variable "sonarqube_host" {
+  description = "The URL of the SonarQube instance"
+  type        = string
+  default     = "http://127.0.0.1:9000"  
+}
+
+variable "sonarqube_token" {
+  description = "The SonarQube authentication token"
+  type        = string
+  sensitive   = true
+  default     = "sqa07537e5f15b6396a61de8144769b935424c3709"  
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "my_project"
+}
+
+variable "project_key" {
+  description = "The key of the project"
+  type        = string
+  default     = "my_project_key"
+}
+
+variable "qualitygate_name" {
+  description = "The name of the quality gate"
+  type        = string
+  default     = "my_qualitygate"
+}
+
+variable "coverage_threshold" {
+  description = "The coverage threshold for the quality gate"
+  type        = number
+  default     = 30
+}
